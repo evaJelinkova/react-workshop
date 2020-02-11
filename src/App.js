19c7f636cc8812fbb045be5dd92f99acbead90da
Eva Jelinkova <evajelinkova@Pavels-Home-Pro.local>
2020-02-11 19:56:58 +0000

    images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import notebook from "./notebook.jpg"
+import titanic from "./titanic.png"
+import madagascar from "./madagascar.jpeg"
+
+function Movie(props) {
+  return (
+    <div>
+<h5>{props.title} (Score: {props.score})</h5>
+        <img src={props.image} />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Best Movies</h1>
+        <Movie title="Notebook" image={notebook} score="9.5"/>
+        <Movie title="Titanic" image={titanic} score="8.9"/>
+        <Movie title="Madagascar" image={madagascar} score="9.3"/>
+    
     </div>
   );
 }
 
 export default App;
+
+
+// ECMAScript -> TC-39
+// Styling standards
+// by AirB&B
